9dc6e5ceaa23417fe170b3af635fb9cf2efda40b
mustafa <msolmaz83@gmail.com>
2024-01-19 23:03:59 +0100

    update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -18,8 +18,12 @@ router.post("/",async(req,res)=>{
         const text = "INSERT INTO users (email, password, fullname) VALUES ($1, crypt($2, gen_salt('bf')), $3) RETURNING *"
 
         const values = [req.body.email, req.body.password, req.body.fullname]
-        const results = await postgresClient.query(text,values)
-        console.log(results);
+       // const results = await postgresClient.query(text,values)
+       // console.log(results);
+       const {rows} = await postgresClient.query(text,values)
+       return res.status(200).json({
+        createdUser:rows[0]
+       })
     } catch (error) {
         console.log("error creee0",error.message)
         return res.status(400).json({
@@ -29,5 +33,45 @@ router.post("/",async(req,res)=>{
 
 })
 
+//auth
+router.post("/login",async(req,res)=>{
+    try {
+        const text = "SELECT * FROM users WHERE email = $1 AND password = crypt($2,password)"
+        const values = [req.body.email,req.body.password]
+        const {rows} = await postgresClient.query(text,values)
+        //console.log(rows)
+        if(!rows.length)  res.status(404).json({message:"user not found"})
+
+        return res.status(200).json({
+            message:"Authetication succes"
+        })
+        
+
+
+
+    } catch (error) {
+        console.log("error",error.message)
+        return res.status(400).json({message:error.message})
+    }
+
+})
+router.put("/update/:userId",async(req,res)=>{
+
+    try {
+        const {userId} = req.params
+        const text = "UPDATE users SET email = $1 ,fullname = $2 WHERE id = $3 RETURNING *"
+        const values = [req.body.email,req.body.fullname,userId]
+        const {rows} = await postgresClient.query(text,values)
+
+        if(!rows.length)  res.status(404).json({message:"user not found"})
+
+        return res.status(200).json({
+            updateUser:rows[0]
+        })
+    } catch (error) {
+        console.log("error",error.message)
+        return res.status(400).json({message:error.message})
+    }
+})
 
 module.exports = router
