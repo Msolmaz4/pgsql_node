b39416c9af8b5bc1393b693d25c9fbb2583c8d4d
mustafa <msolmaz83@gmail.com>
2024-01-20 00:03:44 +0100

    delete

---
+++ README.md
@@ -0,0 +1,25 @@
+
+```
+create User dDOC
+https://node-postgres.com/features/queriesconst
+ text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+const values = ['brianc', 'brian.m.carlson@gmail.com']
+ const res = await client.query(text, values)
+
+https://hasura.io/learn/database/postgresql/installation/2-postgresql-connection-string/
+burdan env baglanti icin postgresql://username:password@host:port/dbname[?paramspec] bunu kullandik
+DB_CONNECTION_STRING=postgresql://postgres:**********@localhost:5432/test localhostu unutma
+https://node-postgres.com/ buda node gore doc
+
+
+
+├─ .env
+├─ db.js
+├─ index.js
+├─ package-lock.json
+├─ package.json
+├─ routes
+│  └─ userRouter.js
+└─ test.rest
+
+```
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,7 @@
-const express = require("express")
+const express = require("express");
 //client kullanmak icin poscClinet cagirdim
-const router = express.Router()
-const postgresClient = require("../db")
-
-
+const router = express.Router();
+const postgresClient = require("../db");
 
 //create User dDOC
 //https://node-postgres.com/features/queries
@@ -11,67 +9,90 @@ const postgresClient = require("../db")
 // const values = ['brianc', 'brian.m.carlson@gmail.com']
 // const res = await client.query(text, values)
 // console.log(res.rows[0])
-router.post("/",async(req,res)=>{
-    console.log(req.body)
-    try {
-        //burda $1 email temsildere password sifelemeyaptogimiy ivcin crypt sarmarladik TRURN UNUTMA GERI DONMESI ICIN
-        const text = "INSERT INTO users (email, password, fullname) VALUES ($1, crypt($2, gen_salt('bf')), $3) RETURNING *"
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    //burda $1 email temsildere password sifelemeyaptogimiy ivcin crypt sarmarladik TRURN UNUTMA GERI DONMESI ICIN
+    const text =
+      "INSERT INTO users (email, password, fullname) VALUES ($1, crypt($2, gen_salt('bf')), $3) RETURNING *";
 
-        const values = [req.body.email, req.body.password, req.body.fullname]
-       // const results = await postgresClient.query(text,values)
-       // console.log(results);
-       const {rows} = await postgresClient.query(text,values)
-       return res.status(200).json({
-        createdUser:rows[0]
-       })
-    } catch (error) {
-        console.log("error creee0",error.message)
-        return res.status(400).json({
-            message:error.message
-        })
-    }
-
-})
+    const values = [req.body.email, req.body.password, req.body.fullname];
+    // const results = await postgresClient.query(text,values)
+    // console.log(results);
+    const { rows } = await postgresClient.query(text, values);
+    return res.status(200).json({
+      createdUser: rows[0],
+    });
+  } catch (error) {
+    console.log("error creee0", error.message);
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+});
 
 //auth
-router.post("/login",async(req,res)=>{
-    try {
-        const text = "SELECT * FROM users WHERE email = $1 AND password = crypt($2,password)"
-        const values = [req.body.email,req.body.password]
-        const {rows} = await postgresClient.query(text,values)
-        //console.log(rows)
-        if(!rows.length)  res.status(404).json({message:"user not found"})
+router.post("/login", async (req, res) => {
+  try {
+    const text =
+      "SELECT * FROM users WHERE email = $1 AND password = crypt($2,password)";
+    const values = [req.body.email, req.body.password];
+    const { rows } = await postgresClient.query(text, values);
+    //console.log(rows)
+    if (!rows.length) res.status(404).json({ message: "user not found" });
 
-        return res.status(200).json({
-            message:"Authetication succes"
-        })
-        
+    return res.status(200).json({
+      message: "Authetication succes",
+    });
+  } catch (error) {
+    console.log("error", error.message);
+    return res.status(400).json({ message: error.message });
+  }
+});
+router.put("/update/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const text =
+      "UPDATE users SET email = $1 ,fullname = $2 WHERE id = $3 RETURNING *";
+    const values = [req.body.email, req.body.fullname, userId];
+    const { rows } = await postgresClient.query(text, values);
 
+    if (!rows.length) res.status(404).json({ message: "user not found" });
 
+    return res.status(200).json({
+      updateUser: rows[0],
+    });
+  } catch (error) {
+    console.log("error", error.message);
+    return res.status(400).json({ message: error.message });
+  }
+});
+router.delete("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const text = "DELETE FROM users WHERE id =$1 RETURNING * ";
+    const values = [userId];
+    const { rows } = await postgresClient.query(text, values);
+    if (!rows.length) res.status(404).json({ message: "user not found" });
 
-    } catch (error) {
-        console.log("error",error.message)
-        return res.status(400).json({message:error.message})
-    }
+    return res.status(200).json({
+      delete: rows[0],
+    });
+  } catch (error) {
+    console.log("error", error.message);
+    return res.status(400).json({ message: error.message });
+  }
+});
 
-})
-router.put("/update/:userId",async(req,res)=>{
+router.get("/", async (req, res) => {
+  try {
+    const text = "SELECT * FROM users ORDER BY id ASC";
+    const { rows } = await postgresClient.query(text);
+    return res.status(200).json(rows);
+  } catch (error) {
+    console.log("error", error.message);
+    return res.status(400).json({ message: error.message });
+  }
+});
 
-    try {
-        const {userId} = req.params
-        const text = "UPDATE users SET email = $1 ,fullname = $2 WHERE id = $3 RETURNING *"
-        const values = [req.body.email,req.body.fullname,userId]
-        const {rows} = await postgresClient.query(text,values)
-
-        if(!rows.length)  res.status(404).json({message:"user not found"})
-
-        return res.status(200).json({
-            updateUser:rows[0]
-        })
-    } catch (error) {
-        console.log("error",error.message)
-        return res.status(400).json({message:error.message})
-    }
-})
-
-module.exports = router
+module.exports = router;
